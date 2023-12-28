0d5ef0482e146cb06cea297a6ad422034665cbd7
stasgigi666 <stanislavgigi@gmail.com>
2023-12-28 18:53:03 +0300

    Updates

--- src/store/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const CHANGE_VALUE = "change-value"
-export const NEXT_LVL = "next-level"
-export const RESTART_LVL = "restart-level"
--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from "redux"
-import reducer from "./reducer"
-
-const reducers = combineReducers({
-    game: reducer
-})
-const store = createStore(reducers)
-export default store
-window.store = store
